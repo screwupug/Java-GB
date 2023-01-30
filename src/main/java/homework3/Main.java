643215d864e1cd90b5b7b3aa729b1d8d1d113a5f
Kirill Grigorev <grigorevkirill@mail.ru>
2023-01-30 22:36:40 +0400

    added homework 3 with comments

--- src/main/java/homework3/Main.java
+++ src/main/java/homework3/Main.java
@@ -1,20 +1,19 @@
 package homework3;
 
-import jdk.jfr.StackTrace;
-
-import javax.sound.midi.Soundbank;
-import java.util.Arrays;
-import java.util.Random;
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Random rnd = new Random();
         int[] a = {2, 9, 5, 4, 8, 1, 6};
-        System.out.println(Arrays.toString(a));
         mergeSort(a);
         System.out.println(Arrays.toString(a));
+        deleteEvenNumbers();
+        minMaxAverage();
+        deleteRepeats();
     }
 
+    // Сортировка слиянием (дробление массива с помощью рекурсии)
     public static void mergeSort(int[] array) {
         int size = array.length;
         if (size == 1) {
@@ -30,13 +29,12 @@ public class Main {
         for (int i = middle; i < size; i++) {
             right[i - middle] = array[i];
         }
-//        System.out.println("mergeSort - left " + Arrays.toString(left));
-//        System.out.println("mergeSort - right " + Arrays.toString(right));
         mergeSort(left);
         mergeSort(right);
         merge(array, left, right);
     }
 
+    // Сортировка слиянием (слияние массивов + сортировка)
     private static void merge(int[] array, int[] left, int[] right) {
         int leftSize = left.length;
         int rightSize = right.length;
@@ -48,12 +46,11 @@ public class Main {
             if (left[leftCount] < right[rightCount]) {
                 array[arrayCount] = left[leftCount];
                 leftCount++;
-                arrayCount++;
             } else {
                 array[arrayCount] = right[rightCount];
                 rightCount++;
-                arrayCount++;
             }
+            arrayCount++;
         }
         if (leftCount < leftSize) {
             System.arraycopy(left, leftCount, array, arrayCount, leftSize - leftCount);
@@ -61,47 +58,75 @@ public class Main {
         if (rightCount < rightSize) {
             System.arraycopy(right, rightCount, array, arrayCount, rightSize - rightCount);
         }
-//        System.out.println(Arrays.toString(array));
-
     }
 
+    // Генератор коллекций
+    public static ArrayList<Integer> generateArray(int size) {
+        Random rnd = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(rnd.nextInt(101));
+        }
+        return list;
+    }
 
-//    public static int[] merge(int[] array) {
-//        int firstBufferSize = array.length / 2;
-//        int secondBufferSize = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
-//        int[] firstBuffer = new int[firstBufferSize];
-//        int[] secondBuffer = new int[secondBufferSize];
-//        int[] result = new int[array.length];
-//        int countForFirstBuffer = 0;
-//        int countForSecondBuffer = 0;
-//        int countForResult = 0;
-//        System.arraycopy(array, 0, firstBuffer, 0, firstBufferSize);
-//        System.out.println(Arrays.toString(firstBuffer));
-//        System.arraycopy(array, firstBufferSize, secondBuffer, 0, secondBufferSize);
-//        System.out.println(Arrays.toString(secondBuffer));
-//        while (countForFirstBuffer < firstBufferSize && countForSecondBuffer < secondBufferSize) {
-//            if (firstBuffer[countForFirstBuffer] < secondBuffer[countForSecondBuffer]) {
-//                result[countForResult] = firstBuffer[countForFirstBuffer];
-//                countForFirstBuffer++;
-//                countForResult++;
-//            } else if (secondBuffer[countForSecondBuffer] < firstBuffer[countForFirstBuffer]) {
-//                result[countForResult] = secondBuffer[countForSecondBuffer];
-//                countForSecondBuffer++;
-//                countForResult++;
-//            }
-//        }
-//        while (countForFirstBuffer < firstBufferSize) {
-//            result[countForResult] = firstBuffer[countForFirstBuffer];
-//            countForFirstBuffer++;
-//            countForResult++;
-//        }
-//        while (countForSecondBuffer < secondBufferSize) {
-//            result[countForResult] = secondBuffer[countForSecondBuffer];
-//            countForSecondBuffer++;
-//            countForResult++;
-//        }
-//        System.out.println(Arrays.toString(result));
-//        return result;
-//
-//    }
+    // Задание 1
+    public static void deleteEvenNumbers() {
+        ArrayList<Integer> list = generateArray(10);
+        for (Integer elem : list) {
+            System.out.printf("%d ", elem);
+        }
+        list.removeIf(integer -> integer % 2 == 0);
+        System.out.println();
+        for (Integer s : list) {
+            System.out.print(s + " ");
+        }
+    }
+
+    // Задание 2
+    public static void minMaxAverage() {
+        ArrayList<Integer> list = generateArray(10);
+        Collections.sort(list);
+        int sum = 0;
+        for (Integer s : list) {
+            System.out.print(s + " ");
+            sum += s;
+        }
+        System.out.println();
+        System.out.printf("Min - %d\n", list.get(0));
+        System.out.printf("Max - %d\n", list.get(list.size() - 1));
+        System.out.printf("Average - %d\n", sum / list.size());
+    }
+
+    // Задание 3
+    public static void deleteRepeats() {
+        String[] planets = new String[]{
+                "Меркурий", "Венера", "Земля",
+                "Марс", "Юпитер", "Сатурн",
+                "Уран", "Нептун"
+        };
+        Random rnd = new Random();
+        ArrayList<String> list = new ArrayList<>();
+        int listSize = rnd.nextInt(10, 26);
+        for (int i = 0; i < listSize; i++) {
+            list.add(planets[rnd.nextInt(planets.length)]);
+        }
+
+        Collections.sort(list);
+        String repeat = list.get(0);
+        for (String item : list) {
+            System.out.printf("%s ", item);
+        }
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).equals(repeat)) {
+                list.remove(list.get(i--));
+            } else {
+                repeat = list.get(i);
+            }
+        }
+        System.out.println();
+        for (String item : list) {
+            System.out.printf("%s ", item);
+        }
+    }
 }
